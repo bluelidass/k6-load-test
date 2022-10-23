c5e556082210b523e49df1d2541f19f5f4e32033
bluelidass <elida.simangunsong@gmail.com>
2022-10-23 12:25:10 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# k6 Load Test
+Getting started? Pls refer to https://k6.io/docs/getting-started/running-k6/
---
+++ scripts/get-list-user.js
@@ -0,0 +1,26 @@
+import { check, sleep } from "k6";
+import http from "k6/http";
+
+export const options = {
+    stages: [
+        {duration: '5m', target: 100},
+        {duration: '10m', target: 100},
+        {duration: '5m', target: 0}
+    ],
+    thresholds: {
+        'http_req_duration': ['p(99)<1500']
+    }
+};
+
+const BASE_URL = 'https://gorest.co.in';
+
+export default () => {
+    const listUser = http.get(`${BASE_URL}/public/v2/users/`);
+
+    check(listUser, {
+        // 'get list user': (resp) => resp.status == 200,
+        'get list user': (obj) => obj.length > 0
+    });
+
+    sleep(1);
+};
